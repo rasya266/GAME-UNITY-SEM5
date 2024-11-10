aa2f2127d7db528a2818c2c4b2780d8894c59353
rasya266 <purnamarizki487@gmail.com>
2024-11-10 14:47:37 +0700

    Update Home Screen Terrain

--- Parkour Game - Minecraft Concept/Assets/Scripts/HomeScript.cs
+++ Parkour Game - Minecraft Concept/Assets/Scripts/HomeScript.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class HomeScript : MonoBehaviour
 {
     public Camera mainCamera;
+    public Transform cT;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        cT = mainCamera.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        mainCamera.transform.Rotate( 0, 10 * Time.deltaTime, 0);
+        cT.rotation = Quaternion.Euler(5, cT.rotation.eulerAngles.y +  10*Time.deltaTime, 0);
     }
 }
